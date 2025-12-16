ad90e528f27aaceb64cc5821cfa8a7caa6bc9803
HilelChemi <hilelchemi@gmail.com>
2025-12-16 14:43:09 +0200

    adding Pice.h

--- Chess_Project/Piece.h
+++ Chess_Project/Piece.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <vector>
+#include <string>
+class Piece {
+private:
+	char symbol;
+	bool isWhite;
+	int coords[2];
+public:
+	Piece(bool isWhite,int courds[]);
+	~Piece();
+	std::vector<int> getCoords(std::string posion);
+	virtual std::vector<std::string> getPosiibleMoves();
+	bool canMove();
+	virtual void changeFirstTurn();
+	bool isKingAttaked();
+	std::string getCurrentPosition();
+	void setCurrentPosition(std::string newPosition);
+	char getSymbol();
+};
