e9750b5b9f5993ee95991857a7dc5af0688ea498
yam tau <yamtau26@gmail.com>
2025-12-16 14:48:58 +0200

    added king h and rook h

--- Chess_Project/King.h
+++ Chess_Project/King.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <Vector>
+#include <string>
+#include "Piece.h"
+
+class King : public Piece
+{
+	public:
+		King();
+		~King();
+		std::vector<std::string> getPossibleMoves() override;
+};
--- Chess_Project/Rook.h
+++ Chess_Project/Rook.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <Vector>
+#include <string>
+#include "Piece.h"
+
+class Rook : public Piece
+{
+	public:
+		Rook();
+		~Rook();
+		std::vector<std::string> getPossibleMoves() override;
+};
