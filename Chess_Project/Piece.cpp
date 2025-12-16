3c7aa4f011a1a34efc747f7ef5e2b41137592cda
HilelChemi <hilelchemi@gmail.com>
2025-12-16 15:34:45 +0200

    done with Piece (Whithout the isKingAttaked)

--- Chess_Project/Piece.cpp
+++ Chess_Project/Piece.cpp
@@ -1,7 +1,68 @@
 #include "Piece.h"
-Piece::Piece(bool isWhite, int coords[],char symble) {
-	this->isWhite = isWhite;
-	char symbol = symble;
-	this->coords[0] = coords[0];
-	this->coords[1] = coords[1];
+
+Piece::Piece(bool isWhite, int coords[], char symbol) //builder
+{
+	_isWhite = isWhite;
+	_symbol = symbol;
+	_coords[0] = coords[0];
+	_coords[1] = coords[1];
+}
+Piece::~Piece()//deconstructor
+{
+}
+
+std::vector<int> Piece::getCoords(std::string& position) const//get coords from position
+{
+	std::vector<int> coords(2);
+	coords[0] =position[0] - 'a';
+	coords[1]= position[1] -  '1';
+	return coords;
+}
+
+bool Piece::canMoveTo(std::string& newPosition) const//check if can move to new position
+{
+	std::vector<std::string> moves = getPossibleMoves();
+	int i = 0;
+	if (newPosition[0] < 'a' || newPosition[0]>'h' || newPosition[1] < '1' || newPosition[1]>'8')
+	{
+		return false;
+	}
+
+	for(i = 0; i < moves.size(); i++)
+	{
+		if (moves[i] == newPosition)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Piece::isKingAttacked() const//check if king is attacked	(check)
+{
+	//later becuse neeed board info
+	return false;
+}
+
+std::string Piece::getCurrentPosition()  const//get current position
+{
+	std::string position = "";
+	position += (char)(_coords[0] + 'a');
+	position += (char)(_coords[1] + '1');
+	return position;
+}
+
+void Piece::setCurrentPosition(std::string& newPosition) //set current position
+{
+	if(canMoveTo(newPosition)==true)
+	{
+		_coords[0] = newPosition[0] - 'a';
+		_coords[1] = newPosition[1] - '1';
+	}
+	
+}
+
+char Piece::getSymbol()  const//get symbol
+{
+	return _symbol;
 }
--- Chess_Project/Piece.h
+++ Chess_Project/Piece.h
@@ -3,18 +3,18 @@
 #include <string>
 class Piece {
 private:
-	char symbol;
-	bool isWhite;
-	int coords[2];
+	char _symbol;
+	bool _isWhite;
+	int _coords[2];
 public:
-	Piece(bool isWhite,int courds[],char symble);
-	~Piece();
-	std::vector<int> getCoords(std::string posion);
-	virtual std::vector<std::string> getPossibleMoves();
-	bool canMoveTo(std::string newPosition);
-	virtual void changeFirstTurn();
-	bool isKingAttacked();
-	std::string getCurrentPosition();
-	void setCurrentPosition(std::string newPosition);
-	char getSymbol();
+	Piece(bool isWhite,int courds[],char symble);//builder
+	~Piece();//deconstructor
+	std::vector<int> getCoords(std::string& posion) const;//get coords from position
+	virtual std::vector<std::string> getPossibleMoves()const =0;//need to be override
+	bool canMoveTo(std::string& newPosition) const;//check if can move to new position
+	virtual void changeFirstTurn()=0;//only for pawns
+	bool isKingAttacked() const;//check if king is attacked	(check)
+	std::string getCurrentPosition() const;//get current position
+	void setCurrentPosition(std::string& newPosition);//set current position
+	char getSymbol() const;//get symbol
 };
